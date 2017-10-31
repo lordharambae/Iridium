af582eb8831217d45dd81b8fc2cff1d2dda23820
KSL <lordharambae@gmail.com>
2017-10-31 09:28:44 +0530

    Create Console.hpp

---
+++ Iridium/IO/Console.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef CONSOLE_HPP
+#define CONSOLE_HPP
+
+namespace Iridium {
+
+	namespace IO {
+	
+		class Console {
+		
+		public:
+		
+			static void log() {}
+			static void warning() {}
+			static void debug() {}
+			static void error() {}
+		
+		};
+	
+	}
+
+}
+
+#endif
