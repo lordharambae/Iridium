474c0e82e5f25affa8cc2892f8d518f4ef01419a
KSL <lordharambae@gmail.com>
2017-10-31 09:23:43 +0530

    Update Scalor.hpp

--- Iridium/Scalor/Scalor.hpp
+++ Iridium/Scalor/Scalor.hpp
@@ -2,23 +2,67 @@
 #ifndef SCALOR_HPP
 #define SCALOR_HPP
 
+#include <iostream>
+
 namespace Iridium {
-
-	namespace Scalor {
-
-		using Bit = bool;
-		using Char8 = char;
-		using Int16 = signed short int;
-		using UInt16 = unsigned short int;
-		using Int32 = signed int;
-		using UInt32 = unsigned int;
-		using Int64 = signed long int;
-		using UInt64 = unsigned long int;
-		using Float32 = float;
-		using Float64 = double;
-		using Float128 = long double;
-
-	} // namespace Scalor
+	
+	template <class T>
+	class Scalor {
+	
+		T scalor;
+		
+	public:
+		
+		Scalor() {}
+		Scalor(const Scalor<T>& right) {}
+		operator T&(const T&& right) {}
+		~Scalor() {}
+		
+		friend bool operator>(const Scalor<T>& left, const Scalor<T>& right) {}
+		friend bool operator>=(const Scalor<T>& left, const Scalor<T>& right) {}
+		friend bool operator==(const Scalor<T>& left, const Scalor<T>& right) {}
+		friend bool operator<=(const Scalor<T>& left, const Scalor<T>& right) {}
+		friend bool operator<(const Scalor<T>& left, const Scalor<T>& right) {}
+		friend bool operator!=(const Scalor<T>& left, const Scalor<T>& right) {}
+		
+		Scalor<T>& operator+(const Scalor<T>& right) const {}
+		Scalor<T>& operator-(const Scalor<T>& right) const {}
+		Scalor<T>& operator*(const Scalor<T>& right) const {}
+		Scalor<T>& operator/(const Scalor<T>& right) const {}
+		Scalor<T>& operator%(const Scalor<T>& right) const {}
+		Scalor<T>& operator^(const Scalor<T>& right) const {}
+		Scalor<T>& operator&(const Scalor<T>& right) const {}
+		Scalor<T>& operator|(const Scalor<T>& right) const {}
+		Scalor<T>& operator~(const Scalor<T>& right) const {}
+		
+		Scalor<T>& operator+=(const Scalor<T>& right) const {}
+		Scalor<T>& operator-=(const Scalor<T>& right) const {}
+		Scalor<T>& operator*=(const Scalor<T>& right) const {}
+		Scalor<T>& operator/=(const Scalor<T>& right) const {}
+		Scalor<T>& operator%=(const Scalor<T>& right) const {}
+		Scalor<T>& operator^=(const Scalor<T>& right) const {}
+		Scalor<T>& operator&=(const Scalor<T>& right) const {}
+		Scalor<T>& operator|=(const Scalor<T>& right) const {}
+		Scalor<T>& operator~=(const Scalor<T>& right) const {}
+		
+		Scalor<T>& operator!(const Scalor<T>& right) const {}
+		Scalor<T>& operator&&(const Scalor<T>& right) const {}
+		Scalor<T>& operator||(const Scalor<T>& right) const {}
+		
+		Scalor<T>& operator<<(Scalor<T>& left, const Scalor<T>& right) {}
+		Scalor<T>& operator>>(Scalor<T>& left, const Scalor<T>& right) {}
+		
+		friend std::ostream& operator<<(std::ostream& left, const Scalor<T>& right) const {}
+		friend std::istream& operator>>(std::istream& left, const Scalor<T>& right) const {}
+	
+	};
+	
+	using Byte = Scalor<bool>;
+	using Short = Scalor<short>;
+	using Integer = Scalor<int>;
+	using Long = Scalor<long>;
+	using Float = Scalor<float>;
+	using Double = Scalor<double>;
 
 } // namespace Iridium
 
