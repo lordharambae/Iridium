99daad5db4f87a247923585e6f123bfc38af02de
KSL <lordharambae@gmail.com>
2017-11-01 13:12:27 +0530

    Update CardinalSinusoid.hpp

--- Iridium/Activation/CardinalSinusoid.hpp
+++ Iridium/Activation/CardinalSinusoid.hpp
@@ -2,6 +2,8 @@
 #ifndef CARDINAL_SINUSOID_HPP
 #define CARDINAL_SINUSOID_HPP
 
+#include "../Scalor/Scalor.hpp"
+
 namespace Iridium {
 
 	namespace Activation {
@@ -9,9 +11,22 @@ namespace Iridium {
 		class CardinalSinusoid {
 
 		public:
-			// Scalor::Float64 Function() {}
+			
+			template<class T>
+			Scalor<T>& Function(const Scalor<T>& x) const {
+				if (x == 0.0) {
+					return 1.0;
+				}
+				return Utility::Math::Sine(x) / Utility::Math::Power(x, 2);
+			}
 
-			// Scalor::Float64 Derivative() {}
+			template<class T>
+			Scalor<T>& Derivative(const Scalor<T>& x) const {
+				if (x == 0.0) {
+					return 0.0;
+ 				}
+ 				return (x * Utility::Math::Cosine(x) - Utility::Math::Sine(x)) / Utility::Math::Power(x, 2);
+			}
 		};
 
 	} // namespace Activation
