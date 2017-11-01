45149d36bf138ed4217ef0fd5d5e3d1bbcf91bae
KSL <lordharambae@gmail.com>
2017-11-01 13:21:47 +0530

    Update Matrix.hpp

--- Iridium/Tensor/Matrix.hpp
+++ Iridium/Tensor/Matrix.hpp
@@ -2,12 +2,16 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
+#include "../Scalor/Scalor.hpp"
+#include "../Utility/Iterator.hpp"
+#include "../Utility/Allocator.hpp"
+
 namespace Iridium {
 
 	namespace Tensor {
 
 		template <class T>
-		using Matrix = std::vector<std::vector<T>>;
+		class Matrix {};
 
 	} // namespace Tensor
 
