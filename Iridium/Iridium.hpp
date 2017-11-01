0f791b3871a5c3f9bb4d94bd38d58b0813ea4b80
KSL <lordharambae@gmail.com>
2017-11-01 13:04:16 +0530

    Update Iridium.hpp

--- Iridium/Iridium.hpp
+++ Iridium/Iridium.hpp
@@ -2,23 +2,18 @@
 #ifndef IRIDIUM_HPP
 #define IRIDIUM_HPP
 
-#include <numeric>
-#include <vector>
-#include <random>
-#include <algorithm>
-#include <functional>
-
 #include "Scalor/Scalor.hpp"
 #include "Tensor/Tensor.hpp"
 #include "Utility/Utility.hpp"
+#include "Kernal/Kernal.hpp"
+#include "IO/IO.hpp"
+#include "Session/Session.hpp"
 #include "Initializer/Initializer.hpp"
 #include "Activation/Activation.hpp"
 #include "Normalizer/Normalizer.hpp"
 #include "Optimizer/Optimizer.hpp"
 #include "Analyzer/Analyzer.hpp"
 #include "Loss/Loss.hpp"
-#include "IO/IO.hpp"
-#include "Session/Session.hpp"
 #include "Model/Model.hpp"
 
 #endif
